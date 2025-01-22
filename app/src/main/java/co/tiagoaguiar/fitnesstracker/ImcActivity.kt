e221853d21c4187af914393d7fe356c6ddf71b06
Renan <renanbezerraenes@icloud.com>
2025-01-22 21:04:16 +0100

    add calculateImc function and log it

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,6 +1,7 @@
 package co.tiagoaguiar.fitnesstracker
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -25,13 +26,21 @@ class ImcActivity : AppCompatActivity() {
                 Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+            val weight = editWeight.text.toString().toInt()
+            val height = editHeight.text.toString().toInt()
 
-
+            val result = calculateImc(weight, height)
+            Log.d("Teste", "result: $result")
         }
     }
 
+    private fun calculateImc(weight: Int, height: Int): Double {
+        //weight / (height * height)
+        //divided by 100.0 to have a double
+        return weight / ((height / 100.0) * (height / 100.0))
+    }
+
     private fun validate(): Boolean {
-        //Better way to return true/false, instead of adding several if/else
         return (editWeight.text.toString().isNotEmpty() && editHeight.text.toString()
             .isNotEmpty() && !editWeight.text.toString()
             .startsWith("0") && !editHeight.text.toString().startsWith("0")

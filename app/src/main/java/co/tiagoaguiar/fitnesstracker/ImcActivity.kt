fd4d7427230941e1295cd89ef60c6e112f471e97
Renan <renanbezerraenes@icloud.com>
2025-01-22 20:49:37 +0100

    add validation in the form

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,11 +1,40 @@
 package co.tiagoaguiar.fitnesstracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+
+        btnSend.setOnClickListener {
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+        }
+    }
+
+    private fun validate(): Boolean {
+        //Better way to return true/false, instead of adding several if/else
+        return (editWeight.text.toString().isNotEmpty() && editHeight.text.toString()
+            .isNotEmpty() && !editWeight.text.toString()
+            .startsWith("0") && !editHeight.text.toString().startsWith("0")
+                )
     }
 }

c59f78525c1aa71b759a6a76ccc8b9017f254f63
Renan <renanbezerraenes@icloud.com>
2025-01-23 22:41:04 +0100

    add recycle view, add items + logic with list

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,23 +1,75 @@
 package co.tiagoaguiar.fitnesstracker
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnImc: LinearLayout
+    private lateinit var rvMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnImc = findViewById(R.id.btn_imc)
+        val mainItems = mutableListOf<MainItem>()
+        mainItems.add(
+            MainItem(
+                id = 1,
+                drawableId = R.drawable.baseline_wb_sunny_24,
+                textStringId = R.string.imc,
+                color = Color.GREEN
+            )
+        )
+        mainItems.add(
+            MainItem(
+                id = 2,
+                drawableId = R.drawable.baseline_wb_sunny_24,
+                textStringId = R.string.tmb,
+                color = Color.BLUE
+            )
+        )
 
-        btnImc.setOnClickListener {
-            //navigate to next page
-            val i = Intent(this, ImcActivity::class.java)
-            startActivity(i)
+        val adapter = MainAdapter(mainItems)
+        rvMain = findViewById(R.id.rv_main)
+        rvMain.adapter = adapter
+        rvMain.layoutManager = LinearLayoutManager(this)
+
+
+    }
+
+    private inner class MainAdapter(private val mainItems: List<MainItem>) : RecyclerView.Adapter<MainViewHolder>() {
+
+        // 1 - specific layout of cel(item of the list)
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+            return MainViewHolder(view)
+        }
+
+        // 2 - Everytime we scroll and it is needed to change the content of the cel(item)
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            val currentItem = mainItems[position]
+            holder.bind(currentItem)
+        }
+
+        // 3 - How many cells this list has
+        override fun getItemCount(): Int {
+            return mainItems.size
+        }
+
+    }
+
+    //individual (cel)
+    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: MainItem){
+            val button: Button = itemView.findViewById(R.id.btn_item)
+            button.setText(item.textStringId)
         }
     }
 }
